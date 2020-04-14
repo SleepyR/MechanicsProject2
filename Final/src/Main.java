3957345e8cdbe70952728487629ea979ace68af2
Rozi Tunyan <SleepyR>
2020-04-14 17:32:05 +0400

    T and a fix

--- Final/src/Main.java
+++ Final/src/Main.java
@@ -1,37 +1,54 @@
 public class Main {
 
-    // From system equations we get
     // Rope  : a1-a2+a3 = 0
     // Hole  : a1x = a3x
-    // a1 = (F + myu1(M1*g+M2*g) +T) / (M1-M3)
-    // a2 = (T + myu2*M2*g )/M2
-    // a3 = (T - M3*g - myu3*M3*a1 )/M3
+
 
 
     public static int g = 10; // m/s^2
 
         public static void main(String[] RTL)
         {
-            double a1;
-            double a2;
-            double a3;
-            double T;
-            Pulley system = new Pulley(100,20,20,0.5,0.5,0.5,-300);
+
+            Pulley system = new Pulley(100,20,20,0.5,0.5,0.5,200);
             double myu1 = system.fric1;
             double myu2 = system.fric2;
             double myu3 = system.fric3;
             double M1= system.M1;
             double M2 = system.M2;
             double M3 = system.M3;
-            int F = system.F;
-            T = (g*(myu2+1)-(1-myu3)*((F+myu1*(M1*g + M2*g))/(M1-M3)))/(1/(M1-M3)+1/M3-1/M2);
+            double F = system.F;
+            coordinates(M1,M2,M3,myu1,myu2,myu3,F,10);
 
-            a1 = (F+myu1*(M1*g+M2*g)+T)/(M1-M3);
+
+        }
+        public static void coordinates(double M1,double M2,double M3, double myu1,double myu2,double myu3, double F, double t)
+        {
+            double a1;
+            double a2;
+            double a3;
+            double T;
+            double x1, x2, x3,y3;
+
+            T = ((1+myu3)/(M1+M3)*(-myu1*g*(M1+M2)-F)+g*(1-myu2))/(1/(M1+M3)*(1+myu1+myu3+myu1*myu3)+1.0/M2+1.0/M3);
+
+            a1 = (-F-myu1*(M1*g+M2*g+T)-T)/(M1+M3);
             a2 = (T+myu2*M2*g)/M2;
             a3 = (T - M3*g - myu3*M3*a1) / M3;
-            System.out.println(T);
-            System.out.println(a1);
-            System.out.println(a2);
-            System.out.println(a3);
+
+            x1 = a1*t*t/2;
+            x2 = a2*t*t/2;
+            x3 = x1;
+            y3 =  a3*t*t/2;
+
+//            System.out.println("T= "+T);
+//            System.out.println("a1= "+a1);
+//            System.out.println("a2= "+a2);
+//            System.out.println("a3= "+a3);
+            System.out.println("X coordinate of M1 :"+x1);
+            System.out.println("X coordinate of M2 :"+x2);
+            System.out.println("X coordinate of M3 :"+x3);
+            System.out.println("Y coordinate of M3 :"+y3);
+            System.out.println("Y coordinates of M1 and M1 didn't change");
         }
 }
